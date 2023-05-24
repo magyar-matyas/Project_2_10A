8cf08ce1965eeb5f6fa43ca3480609da84899931
kristof-anda <anda.kristof@students.jedlik.eu>
2023-05-24 18:26:17 +0200

    osztaly kesz

---
+++ Corpses.py
@@ -0,0 +1,17 @@
+import pygame
+
+class Corpses:
+    kirajz = []
+    
+    def __init__(self, szint) -> None:
+        self.kirajz.append(pygame.image.load(f'akasztottak/lvl{szint}/h0.png'))
+        self.kirajz.append(pygame.image.load(f'akasztottak/lvl{szint}/h1.png'))
+        self.kirajz.append(pygame.image.load(f'akasztottak/lvl{szint}/h2.png'))
+        self.kirajz.append(pygame.image.load(f'akasztottak/lvl{szint}/h3.png'))
+        self.kirajz.append(pygame.image.load(f'akasztottak/lvl{szint}/h4.png'))
+        self.kirajz.append(pygame.image.load(f'akasztottak/lvl{szint}/h5.png'))
+        self.kirajz.append(pygame.image.load(f'akasztottak/lvl{szint}/h6.png'))
+        self.kirajz.append(pygame.image.load(f'akasztottak/lvl{szint}/h7.png'))
+        self.kirajz.append(pygame.image.load(f'akasztottak/lvl{szint}/h8.png'))
+        
+        
