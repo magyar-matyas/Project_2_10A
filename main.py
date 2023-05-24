ef62e34d933b1fedec7a2bf7982dce3d5663c2bc
kristof-anda <anda.kristof@students.jedlik.eu>
2023-05-24 16:08:43 +0200

    pygame, screen

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
+import pygame
+
+
 def main() -> None:
     
+    screen = pygame.display.set_mode((1200, 600))
+    pygame.display.set_caption('Hangman')
+    clock = pygame.time.Clock()
+    
+    
+    while True:
+    
+        for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    exit()
 
-
+        pygame.display.update()
+        clock.tick(60)
 if __name__ == "__main__":
     main()
